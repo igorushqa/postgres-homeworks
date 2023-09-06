e9f24c5804e84d4a57166bba28397e3680b02542
igorushqa <since1925@mail.ru>
2023-09-06 22:31:18 +0700

    HW1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title text,
+	birth_date varchar(25) NOT NULL,
+	notes varchar(100) NOT NULL
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar(25) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer varchar(25) REFERENCES customers(customer_id) NOT NULL,
+	employee int REFERENCES employees(employee_id) NOT NULL,
+	order_date varchar(100) NOT NULL,
+	ship_city varchar(100) NOT NULL
+)
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,40 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import psycopg2
+
+
+def start():
+    conn = psycopg2.connect(
+        host="localhost",
+        database="north",
+        user="postgres",
+        password="kirova7574"
+    )
+
+    data_customers = '''COPY customers(customer_id,company_name,contact_name)
+    FROM 'C:/Users/Since1925/PycharmProjects/postgres-homeworks/homework-1/north_data/customers_data.csv'
+    DELIMITER ','
+    CSV HEADER;'''
+
+    data_employees = '''COPY employees(employee_id,first_name,last_name,title,birth_date,notes)
+    FROM 'C:/Users/Since1925/PycharmProjects/postgres-homeworks/homework-1/north_data/employees_data.csv'
+    DELIMITER ','
+    CSV HEADER;'''
+
+    data_orders = '''COPY orders(order_id,customer,employee,order_date,ship_city)
+    FROM 'C:/Users/Since1925/PycharmProjects/postgres-homeworks/homework-1/north_data/orders_data.csv'
+    DELIMITER ','
+    CSV HEADER;'''
+
+    try:
+        with conn:
+            with conn.cursor() as cur:
+                cur.execute(data_customers)
+                cur.execute(data_employees)
+                cur.execute(data_orders)
+    finally:
+        conn.close()
+
+
+if __name__ == '__main__':
+    start()
