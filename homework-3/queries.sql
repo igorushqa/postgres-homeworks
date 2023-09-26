62970fd8552ac065cae53ac2a864ad9ba29b2f22
igorushqa <since1925@mail.ru>
2023-09-27 00:55:32 +0700

    HW3

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,16 +1,31 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-
+SELECT company_name as customer, CONCAT(first_name, ' ', last_name) as employee
+FROM customers
+INNER JOIN employees USING(city)
+WHERE city IN ('London')
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
-
+SELECT product_name, units_in_stock, suppliers.contact_name, suppliers.phone
+FROM products
+INNER JOIN suppliers USING(supplier_id)
+WHERE discontinued=0 AND category_id in ('2','4')
+GROUP BY product_name, units_in_stock, suppliers.contact_name, suppliers.phone
+HAVING units_in_stock < 25
+ORDER BY units_in_stock 
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
-
+SELECT company_name
+FROM customers
+LEFT JOIN orders USING(customer_id)
+WHERE orders.customer_id is null
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+SELECT DISTINCT product_name
+FROM products
+WHERE EXISTS (SELECT * FROM order_details WHERE quantity=10 AND products.product_id=order_details.product_id)
